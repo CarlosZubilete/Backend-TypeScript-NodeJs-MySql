61c681a41db854870fd6854c79aed9da402ebcc0
CZubileteSanchez <carloszubilete@gmail.com>
2025-10-23 07:03:15 -0300

    feat: create a new function that it allows handle error and works as a middleware for login and singup. Delete try and catch at the controller.ts

--- src/controllers/controller.auth.ts
+++ src/controllers/controller.auth.ts
@@ -2,7 +2,8 @@ import { NextFunction, Request, Response } from "express";
 import { prismaClient } from "..";
 import { compareSync, hashSync } from "bcrypt";
 import { JWT_SECRET, SALT_ROUNDS } from "../secrets";
-import * as jwt from "jsonwebtoken";
+//import * as jwt from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 import { BadRequestException } from "../exceptions/exception.bad-request";
 import { ErrorCode } from "../exceptions/exception.root";
 import { UnprocessableEntity } from "../exceptions/exception.validation";
@@ -13,35 +14,35 @@ export const signup = async (
   res: Response,
   next: NextFunction
 ) => {
-  try {
-    schemaSignup.parse(req.body);
-    const { name, email, password } = req.body;
-    let user = await prismaClient.user.findFirst({ where: { email } });
-    if (user) {
-      next(
-        new BadRequestException(
-          "User already exists!",
-          ErrorCode.USER_ALREADY_EXISTS
-        )
-      );
-    }
-    user = await prismaClient.user.create({
-      data: {
-        name,
-        email,
-        password: hashSync(password, parseInt(SALT_ROUNDS)),
-      },
-    });
-    res.status(200).json({ ok: true, name });
-  } catch (err: any) {
+  // try {
+  schemaSignup.parse(req.body);
+  const { name, email, password } = req.body;
+  let user = await prismaClient.user.findFirst({ where: { email } });
+  if (user) {
     next(
-      new UnprocessableEntity(
-        "Unprocessable entity",
-        ErrorCode.UNPROCESSABLE_ENTITY,
-        err?.issues
+      new BadRequestException(
+        "User already exists!",
+        ErrorCode.USER_ALREADY_EXISTS
       )
     );
   }
+  user = await prismaClient.user.create({
+    data: {
+      name,
+      email,
+      password: hashSync(password, parseInt(SALT_ROUNDS)),
+    },
+  });
+  res.status(200).json({ ok: true, name });
+  // } catch (err: any) {
+  //   next(
+  //     new UnprocessableEntity(
+  //       "Unprocessable entity",
+  //       ErrorCode.UNPROCESSABLE_ENTITY,
+  //       err?.issues
+  //     )
+  //   );
+  // }
 };
 
 // TODO: all async function have got inside try and catch
